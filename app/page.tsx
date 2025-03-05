6117d563211a649c7473918724a328d6e082af6b
ximenabb <78557542+ximenabb@users.noreply.github.com>
2025-03-05 12:57:40 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -14,9 +14,9 @@ import Textarea from "react-textarea-autosize";
 import Image from "next/image";
 
 const examples = [
-  "Example 1",
-  "Example 2",
-  "Example 3",
+  "Hi there!",
+  "Welcome!",
+  "Create an observation",
 ];
 
 export default function Chat() {
@@ -83,7 +83,7 @@ export default function Chat() {
               className="h-20 w-20"
             />
             <h1 className="text-lg font-semibold text-black">
-              Hi, I'm a fine tuned LLM.
+              Hi, I'm a CoMapeo chatbot.
             </h1>
             <p className="text-gray-500">
               I'm part of a series of computational experiments taught by{" "}
